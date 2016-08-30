634c98c010c4d35d9bcab11d0e0665675605881b
Luke <lgriffith@redventures.com>
2016-08-30 16:27:02 -0400

    warblgarbl

--- public/client/src/sagas/messages.js
+++ public/client/src/sagas/messages.js
@@ -5,11 +5,7 @@ import { browserHistory } from 'react-router';
 
 function* getMsg(action) {
   try{
-    let convos = yield Conversations.find({ to: action.user }).fetch()
-    yield put({
-      type: 'GET_MESSAGES_SUCCESS',
-      payload: convos
-    });
+    
   } catch(err) {
     console.log('AHHHHH no')
   }
@@ -18,25 +14,7 @@ function* getMsg(action) {
 
 function* sendMsg(action) {
   try{
-    let newMsg = {
-      to: action.payload.view.owner._id,
-      owner: {
-        _id: Meteor.user()._id,
-        name: Meteor.user().profile.firstName
-      },
-      message: action.payload.message
-    }
-    const res = yield Meteor.call('sendMessage', newMsg, action.payload.view._id, (err, result) => {
-      console.log('result? ', result)
-      return result
-    })
-    console.log('res: ', res)
-    const msg = yield Conversations.find({ _id: action.payload.view._id }).fetch();
-    console.log('the msg: ', msg)
-    yield put({
-      type: 'UPDATE_CONVERSATION',
-      payload: msg[0]
-    });
+    
   } catch(err) {
     console.log('daaaaaaang dude try again')
   }
